defd2971717e74338b004098b4a17fd3b3102a49
Nikola Stojanović <153657652+Stojanovic94@users.noreply.github.com>
2025-01-29 10:53:38 +0100

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,12 +1,9 @@
 <?php
-// Podaci za povezivanje sa bazom podataka
 $servername = "localhost";
 $username = "root";
-$password = ""; // Prazno polje, jer XAMPP podrazumevano nema lozinku za root korisnika
+$password = "";
 $dbname = "fakultet";
-// Kreiranje konekcije
 $conn = new mysqli($servername, $username, $password, $dbname);
-// Provera konekcije
 if ($conn->connect_error) {
 die("Konekcija nije uspela: " . $conn->connect_error);
 }
